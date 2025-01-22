64436986edb3730ed695bf820c75fb0301096de2
ZahraZabihin <z.zabihin@oddifact.com>
2025-01-22 11:03:52 +0000

    myenv

--- extractor_utils.py
+++ extractor_utils.py
@@ -2,7 +2,7 @@
 
 import os
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI 
+from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage, SystemMessage
 
 # Load environment variables
